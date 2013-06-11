f5f8aef59acd1e4a8e3a73383d563e7ff7a69e61
Ken Heutmaker <kheutmaker@rallydev.com>
2013-06-10 17:16:33 -0700

    Update to latest.

--- js/iron-triangle.js
+++ js/iron-triangle.js
@@ -1,40 +1,98 @@
 (function() {
-  var balance, controls, cost, formula, quality, results, solver, time;
+  var balance, controlList, controls, cost, createSlider, formula, quality, solver, time,
+    _this = this;
 
   c.debug = true;
 
   c.trace = true;
 
-  solver = new c.SimplexSolver();
+  controlList = [];
+
+  solver = new c.SimplexSolver;
+
+  solver.onsolved = function() {
+    return _.each(controlList, function(meta) {
+      return meta.control.slider('value', meta.variable.value);
+    });
+  };
 
   cost = new c.Variable({
-    name: 'cost'
+    name: 'cost',
+    value: 0
   });
+
+  solver.addConstraint(new c.Inequality(cost, c.GEQ, -100));
+
+  solver.addConstraint(new c.Inequality(cost, c.LEQ, 100));
 
   quality = new c.Variable({
-    name: 'quality'
+    name: 'quality',
+    value: 0
   });
 
+  solver.addConstraint(new c.Inequality(quality, c.GEQ, -100));
+
+  solver.addConstraint(new c.Inequality(quality, c.LEQ, 100));
+
   time = new c.Variable({
-    name: 'time'
+    name: 'time',
+    value: 0
   });
+
+  solver.addConstraint(new c.Inequality(time, c.GEQ, -100));
+
+  solver.addConstraint(new c.Inequality(time, c.LEQ, 100));
 
   formula = c.plus(cost, c.plus(quality, time));
 
-  balance = new c.Equation(formula, 1, c.Strength.required, 0);
+  balance = new c.Equation(formula, 0, c.Strength.required, 0);
 
   solver.addConstraint(balance);
 
-  results = $('#results');
+  solver.addEditVar(cost, c.Strength.strong).beginEdit();
 
-  $("<p>cost: " + cost.value + "</p>").appendTo(results);
+  solver.addEditVar(quality, c.Strength.strong).beginEdit();
 
-  $("<p>quality: " + quality.value + "</p>").appendTo(results);
+  solver.addEditVar(time, c.Strength.strong).beginEdit();
 
-  $("<p>time: " + time.value + "</p>").appendTo(results);
+  solver.suggestValue(cost, 0);
+
+  solver.suggestValue(quality, 0);
+
+  solver.suggestValue(time, 0);
+
+  solver.endEdit();
 
   controls = $('#controls');
 
-  $('<p>hello controls</p>').appendTo(controls);
+  createSlider = function(model) {
+    var slider,
+      _this = this;
+    slider = $('<span class="slider-control"/>').slider({
+      min: -100,
+      max: 100,
+      value: model.value,
+      start: function(event, ui) {
+        return solver.addEditVar(model, c.Strength.high).beginEdit();
+      },
+      stop: function(event, ui) {
+        return solver.endEdit();
+      },
+      slide: function(event, ui) {
+        return solver.suggestValue(model, ui.value).resolve();
+      }
+    });
+    controlList.push({
+      variable: model,
+      control: slider
+    });
+    return $("<div class='slider-container clearfix'><span class='slider-label'>" + model.name + "</span></div>").append(slider);
+  };
+
+  createSlider(cost).appendTo(controls);
+
+  createSlider(quality).appendTo(controls);
+
+  createSlider(time).appendTo(controls);
 
 }).call(this);
